f0a200f0fd3eaf4cd132c76a26ca281152a1f9a5
Anish Limbu <54665255+LimbuAnish@users.noreply.github.com>
2026-01-16 02:29:27 +0545

    Some-Changes

--- ChaseMultiplayerGame/Assets/Scripts/PlayerMovement.cs
+++ ChaseMultiplayerGame/Assets/Scripts/PlayerMovement.cs
@@ -15,6 +15,12 @@ public class PlayerMovement : NetworkBehaviour
     bool isGrounded = false;
     bool isJump = false;
 
+    PlayerController controller;
+    private void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
     void Update()
     {
         // IsOwner will also work in a distributed-authoritative scenario as the owner 
@@ -31,9 +37,9 @@ public class PlayerMovement : NetworkBehaviour
 
         if (dir.magnitude > 0.5f)
         {
-           lastDirection = dir;
+            lastDirection = dir;
         }
-        transform.forward = Vector3.Lerp(transform.forward, lastDirection,0.2f);
+        transform.forward = Vector3.Lerp(transform.forward, lastDirection, 0.2f);
         // transform.position += dir * Speed * Time.deltaTime;
     }
     private void FixedUpdate()
@@ -41,13 +47,23 @@ public class PlayerMovement : NetworkBehaviour
         if (!IsOwner || !IsSpawned) return;
 
         //Checks the collision groinded or not
-        if (Physics.OverlapBox(footPoint.transform.position, new Vector3(0.6f, 0.6f, 0.6f),quaternion.identity,LayerMask.GetMask("Ground")).Length > 0) isGrounded = true;
+        if (CheckGround("Ground") || CheckGround("Water")) isGrounded = true;
         else isGrounded = false;
 
         //Acceleration
-        float speed = Mathf.Clamp(rb.linearVelocity.magnitude, 30, 60);
+        float speed = 0;
+        if (CheckGround("Water") && !controller.IsInfected)
+        {
+
+            speed = Mathf.Clamp(rb.linearVelocity.magnitude, 7, 7);
+        }
+        else
+        {
+            speed = Mathf.Clamp(rb.linearVelocity.magnitude, 12, 15);
+        }
+
         //Deacceleration
-        if (dir.magnitude == 0 && isGrounded) rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, Vector3.zero,0.1f);
+        if (dir.magnitude == 0 && isGrounded) rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, Vector3.zero, 0.1f);
 
         //Movement
         //Applying Acceleration less while in Air
@@ -55,11 +71,17 @@ public class PlayerMovement : NetworkBehaviour
         rb.AddForce(dir * speed, ForceMode.Acceleration);
 
         //Gravity pushes downards by more force when in a air
-        rb.AddForce(Physics.gravity * Mathf.Lerp(0,30,0.05f), ForceMode.Acceleration);
+        if (!isGrounded) rb.AddForce(Physics.gravity * Mathf.Lerp(0, 30, 0.05f), ForceMode.Acceleration);
     }
+
+    private bool CheckGround(string layer)
+    {
+        return Physics.OverlapBox(footPoint.transform.position, new Vector3(0.6f, 0.6f, 0.6f), quaternion.identity, LayerMask.GetMask(layer)).Length > 0;
+    }
+
     private void Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             var jumpForce = 15f;
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
